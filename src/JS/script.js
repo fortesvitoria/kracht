dd630f5c983cffa21254cbb05f7b969a84237f74
Vitoria Fortes <fortesvitoria@gmail.com>
2024-11-08 13:48:23 -0300

    atualizando cards bicicletas

--- src/JS/script.js
+++ src/JS/script.js
@@ -7,9 +7,10 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $('.carousel-bikes').slick({
-      infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
     });
   
   });
