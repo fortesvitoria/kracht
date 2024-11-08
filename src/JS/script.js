429e3b5a7ff4c2d7efe28fcb96a97fed3bba7f69
Vitoria Fortes <fortesvitoria@gmail.com>
2024-11-08 14:15:51 -0300

    ajustando icones  e cards

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,6 +1,7 @@
 // carousel info-bike
 $(document).ready(function(){
     $('.carousel-info-bike').slick();
+    
   });
 
   //carousel-bikes
@@ -14,5 +15,17 @@ $(document).ready(function(){
     });
   
   });
+
+    //carousel-roupas
+
+    $(document).ready(function(){
+      $('.carousel-roupas').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+    
+    });
 
 
