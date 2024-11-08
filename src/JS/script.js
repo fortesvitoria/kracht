2f5eda6abb4508222cb21d4cdda63c9eb05bbd3e
Vitoria Fortes <fortesvitoria@gmail.com>
2024-11-08 11:01:03 -0300

    adiciona imagens cards bikes

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,6 +1,17 @@
+// carousel info-bike
 $(document).ready(function(){
     $('.carousel-info-bike').slick();
   });
 
+  //carousel-bikes
+
+  $(document).ready(function(){
+    $('.carousel-bikes').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+  
+  });
 
 
